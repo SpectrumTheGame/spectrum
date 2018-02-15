612b29a2a98a781f396e704caa31ccd31e54145b
Vincent Chen <vincentschen@yahoo.com>
2018-02-14 23:49:25 -0800

    Add starter code / simple force

---
+++ Assets/AddForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour {
+
+	private Rigidbody2D rb; 
+		
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce(transform.right * 5.0f);
+
+	}
+}
