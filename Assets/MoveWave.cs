5cb9a5a36ce10640caf8cb0621b9e543117f46f6
Kristen Law <kristenlaw.2014@gmail.com>
2018-02-19 14:52:06 -0800

    multiple forces from spawner

---
+++ Assets/MoveWave.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWave : MonoBehaviour {
+
+	GameObject ball;
+	Rigidbody2D rb;
+	bool isLeft;
+
+	// Use this for initialization
+	void Start () {
+		ball = GameObject.Find ("Ball");
+		Debug.Log (ball.transform.position);
+		rb = ball.GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0.1f, 0, 0);
+
+		Vector3 diff = transform.position - ball.transform.position;
+		if (diff.magnitude < 1) {
+			rb.AddForce(transform.right * 5.0f);
+		}
+
+		if (transform.position.x > 7) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/SpawnForce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnForce : MonoBehaviour {
+
+	public GameObject wavePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Spawn () {
+		GameObject wave = Instantiate (wavePrefab, transform.position, transform.rotation);
+//		wave.AddComponent
+//		wave.transform.Translate (20f, 0.5f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Spawn ();
+		}
+	}
+}
