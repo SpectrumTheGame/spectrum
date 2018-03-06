52bc543e15d764d4db6ba118be9b8e624f031aa3
unknown <hsun083@gmail.com>
2018-03-05 16:46:01 -0800

    Fixed fatter wave widths

--- Assets/SpawnForce.cs
+++ Assets/SpawnForce.cs
@@ -21,13 +21,13 @@ public class SpawnForce : MonoBehaviour {
 		GameObject wave = Instantiate (wavePrefab, newPos, midpoint.transform.rotation);
 		wave.transform.localPosition = new Vector3(wave.transform.localPosition.x, wave.transform.localPosition.y, 0.0f);
         wave.GetComponent<MoveWave>().amplitude = amplitude;
-		wave.transform.localScale = new Vector3(1.0f/waveLen, waveLen, 1); 
+		wave.transform.localScale = new Vector3(0.1f*waveLen, waveLen, 1); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (triggered) {
-			Spawn ();
+			Spawn();
 			triggered = false; 
 		}
 	}
