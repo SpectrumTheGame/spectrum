5001c08b0add82df1b6ffe62bfe19f93e03ef261
Vincent Chen <vincentschen@yahoo.com>
2018-03-08 15:52:08 -0800

    Blow up goal when win

--- Assets/MoveWave.cs
+++ Assets/MoveWave.cs
@@ -15,22 +15,33 @@ public class MoveWave : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		ball = GameObject.Find ("ball");
-		rb = ball.GetComponent<Rigidbody2D> ();
+		if (ball != null) {
+			rb = ball.GetComponent<Rigidbody2D> ();
+			ballRenderer = ball.GetComponent<Renderer> ();
+		}
+		
         applyingForce = false;
 		waveRenderer = GetComponent<Renderer>();
-		ballRenderer = ball.GetComponent<Renderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// ball destroyed when reaches goal/transitioning
+
 		transform.Translate(0.1f * -amplitude, 0, 0);
 
+		// don't iteract with the ball if ball doesn't exist
+		if (ball == null)
+			return; 
+
+		// wave interaction 
 		if (waveRenderer.bounds.Intersects (ballRenderer.bounds) 
 			&& !applyingForce) {
             applyingForce = true;
 			rb.AddForce(transform.right * 100.0f * -amplitude);
 		} 
 
+		// destroy the wave when it moves out of bounds
 		if (transform.position.x > 10 || transform.position.x < -10
 			|| transform.position.y > 10 || transform.position.y < -10) {
 			Destroy (this.gameObject);
--- Assets/ReachedGoal.cs
+++ Assets/ReachedGoal.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class ReachedGoal : MonoBehaviour {
 
-	private GameObject ball; 
+	private GameObject ball;
+	public bool isTransitioning = false;
+	public float scaleSpeed = 0.3F;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (isTransitioning) {
+			Destroy (ball);
+			blowUpGoal();
+		}
 		
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		
 		if (collision.gameObject.name == "ball") {
-			Debug.Log ("ball collided");
-			Destroy (gameObject);
+			ball = collision.gameObject;
+			Debug.Log ("ball collided!!");
+			isTransitioning = true; 
 		}
 	}
+
+	void blowUpGoal() {
+		// disable rigidbody behavior on goal (sliding reaction to forces)
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		Destroy (rb);
+
+		// center the goal 
+		float distanceFromCamera = 10.0f;
+		Vector3 goalPosition = transform.position;
+		Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, distanceFromCamera));
+		transform.position += (centerPos - goalPosition) / 40.0F;
+
+		// blow up the goal 
+		Vector3 scaleIncrease = new Vector3(1.6F, 0.9F, 0); // set to aspect ratio of iphone7
+		Vector3 maxScale = scaleIncrease * 5.0F;
+		transform.localScale += scaleIncrease * scaleSpeed;
+		Debug.Log (transform.localScale);
+
+		// reset flag when target position/size achieved
+		if (transform.position == centerPos && transform.localScale.x > maxScale.x) {
+			isTransitioning = false;
+		}
+
+	}
+
 }
