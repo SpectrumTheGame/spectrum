259fd7539a886f2664111ffa392e0209ad1bc8a6
unknown <hsun083@gmail.com>
2018-03-08 23:10:53 -0800

    beginning portal level + functionality

---
+++ Assets/PortalBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalBehavior : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject otherPortal;
+    public Vector3 inwards;
+
+    private Vector2 vel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vel = ball.GetComponent<Rigidbody2D>().velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == ball)
+        {
+            
+            ball.transform.position = otherPortal.transform.position + (0.2f * inwards);
+            ball.GetComponent<Rigidbody2D>().velocity = vel.magnitude * inwards;
+        }
+    }
+}
