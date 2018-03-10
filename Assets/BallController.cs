2e31c6c9d6123be49ea5ab23259254ac4b52dd13
unknown <hsun083@gmail.com>
2018-03-09 17:50:44 -0800

    resets after 2 seconds when it's out of the screen

---
+++ Assets/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    IEnumerator Pause()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        Camera.main.GetComponent<changeScene>().resetScene();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (Math.Abs(pos.x) > Screen.width)
+        {
+            StartCoroutine(Pause());
+        }
+
+        if (Math.Abs(pos.y) > Screen.height)
+        {
+            StartCoroutine(Pause());
+        }
+    }
+}
