ea59920724bb09e6bb0b5af3a92984d62fd9ba4e
unknown <hsun083@gmail.com>
2018-03-09 16:34:47 -0800

    added replay

---
+++ Assets/ReplaySounds.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySounds : MonoBehaviour {
+
+
+    //private AudioSource curr;
+    private float t;
+
+    
+	// Use this for initialization
+	void Start () {
+	}
+
+    IEnumerator Play(AudioSource curr, int i)
+    {
+        yield return new WaitForSeconds(2 * i);
+        curr.Play();
+    }
+
+    public void PlayAllSounds()
+    {
+        int i = 0;
+        foreach (var audio in Camera.main.gameObject.GetComponents<AudioSource>())
+        {
+            StartCoroutine(Play(audio, i));
+            i++;
+        }
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space"))
+        {
+            PlayAllSounds();
+        }
+	}
+}
