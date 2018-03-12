00e86c52d158ee1ec0478beb3d9d7fb179eb1b64
Da Eun Kim <daeunkim@DN0a1f6d51.SUNet>
2018-03-11 17:49:09 -0700

    updated the BallController script to reset when out of bounds on all 4 sides

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,29 +6,27 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {		
 	}
 
     IEnumerator Pause()
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.0f);
 
         Camera.main.GetComponent<changeScene>().resetScene();
     }
 
     // Update is called once per frame
     void Update () {
-        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+		// Got rid of Math.abs because doesn't seem needed?
+		if (pos.x > Screen.width || pos.x < 0.0f)
+		{
+			StartCoroutine(Pause());
+		}
 
-        if (Math.Abs(pos.x) > Screen.width)
-        {
-            StartCoroutine(Pause());
-        }
-
-        if (Math.Abs(pos.y) > Screen.height)
-        {
-            StartCoroutine(Pause());
-        }
+		if (pos.y > Screen.height || pos.y < 0.0f)
+		{
+			StartCoroutine(Pause());
+		}
     }
 }
