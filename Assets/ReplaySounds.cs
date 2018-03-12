b4dcc387c979ed792ac11c0de694a85a96d82934
unknown <hsun083@gmail.com>
2018-03-12 14:43:55 -0700

    made playback faster

--- Assets/ReplaySounds.cs
+++ Assets/ReplaySounds.cs
@@ -15,7 +15,7 @@ public class ReplaySounds : MonoBehaviour {
 
     IEnumerator Play(AudioSource curr, int i)
     {
-        yield return new WaitForSeconds(1 * i);
+        yield return new WaitForSeconds(0.3f * i);
         curr.Play();
     }
 
