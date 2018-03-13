f3d205dcdf3be2c2c29960bc21b59b63d5134136
Kristen Law <kristenlaw.2014@gmail.com>
2018-03-13 15:52:17 -0700

    Add portal animation

---
+++ Assets/PulsingAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsingAnimation : MonoBehaviour {
+
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float timeElapsed = Time.time - startTime;
+		float scale = 4.8f + Mathf.Sin (5 * timeElapsed)/2;
+		transform.localScale = new Vector3 (scale, scale, 0);
+	}
+}
